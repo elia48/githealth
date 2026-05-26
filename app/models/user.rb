484d9866faba7c2e159e69b4ff9d781b7364136a
Juan Espinar <juanxavierespinar@gmail.com>
2026-05-26 12:07:57 +0200

    rollback migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :daily_plans, dependent: :destroy
+  has_many :messages
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
