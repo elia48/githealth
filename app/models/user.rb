0a218d695454516b8577f38b4b7de83a6cbfe2aa
Juan Espinar <juanxavierespinar@gmail.com>
2026-05-26 12:08:37 +0200

    eliminate user

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :daily_plans, dependent: :destroy
-  has_many :messages
-
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-end
