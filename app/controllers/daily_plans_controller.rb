6378c5413bc41f79c599a5284f5a6348617356c2
Elia Mangano <eliamangano@gmail.com>
2026-05-27 16:04:54 +0200

    little fix added calendar wip

--- app/controllers/daily_plans_controller.rb
+++ app/controllers/daily_plans_controller.rb
@@ -3,6 +3,10 @@ class DailyPlansController < ApplicationController
     @daily_plans = DailyPlan.all
   end
 
+  def show
+    @daily_plan = DailyPlan.find(params[:id])
+  end
+
   def new
     @daily_plan = DailyPlan.new
   end
@@ -16,23 +20,24 @@ class DailyPlansController < ApplicationController
     @daily_plan = DailyPlan.find(params[:id])
   end
 
-def update
-  @daily_plan = DailyPlan.find(params[:id])
+  def update
+    @daily_plan = DailyPlan.find(params[:id])
 
-  if @daily_plan.update(check_in_params)
-    redirect_to daily_plan_path(@daily_plan),
-    notice: "Daily check-in completed!"
-  else
-    render :edit, status: :unprocessable_entity
+    if @daily_plan.update(check_in_params)
+      redirect_to daily_plan_path(@daily_plan),
+      notice: "Daily check-in completed!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-end
 
   private
 
   def check_in_params
-  params.require(:daily_plan).permit(:actual_sleep, :actual_workout_duration, :actual_workout_type, :actual_nutrition, :workout_feeling)
+    params.require(:daily_plan).permit(:actual_sleep, :actual_workout_duration, :actual_workout_type, :actual_nutrition, :workout_feeling)
   end
 
   def daily_plan_params
     params.require(:daily_plans).permit(:planned_sleep, :planned_workout_duration,:planned_workout_type, :planned_nutrition_log)
   end
+end
